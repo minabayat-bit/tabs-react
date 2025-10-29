03cef2c862644ba67e549264401e8629f7fda888
minabayat <minabayatt9@gmail.com>
2025-10-29 14:42:38 +0330

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -8,28 +8,29 @@ function App() {
 
   return (
     <>
-      <div className=" h-full flex lg:flex-col flex-wrap justify-center items-center gap-5">
-        <h2 className="lg:text-4xl tracking-[2px] text-3xl font-semibold text-gray-800">About</h2>
-        <p className="text-gray-600 text-base lg:text-[20px]">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
-          aperiam!
+      <div className="h-full flex flex-col justify-center items-center gap-5 px-4 lg:px-0">
+        <h2 className="text-3xl lg:text-5xl font-bold tracking-[2px] text-gray-800">About</h2>
+        <p className="text-base lg:text-[20px] text-gray-600 text-center lg:text-left">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, aperiam!
         </p>
-        <div className="flex flex-col gap-5 lg:flex-row mt-5">
+        <div className="flex flex-col lg:flex-row gap-5 mt-5 w-full max-w-[1150px]">
           <img
-            className="lg:w-[569px] w-full h-[300px] rounded-lg"
+            className="w-full lg:w-[569px] h-[200px] lg:h-[300px] rounded-lg object-cover"
             src=".\src\assets\img\hero-bcg.1486d4061cd0252b9a21.jpeg"
             alt=""
           />
-          <div className="flex flex-col">
-            <div className="bg-white lg:w-[570px] w-full h-auto rounded-lg ">
-              <div className="flex flex-row justify-around">
+          <div className="flex flex-col w-full lg:w-[570px]">
+            <div className="bg-white w-full h-auto rounded-lg">
+              <div className="flex flex-col sm:flex-row justify-around items-center gap-3 sm:gap-0 ">
                 <button
                   onClick={() => {
                     setHistory(true);
                     setVision(false);
                     setGoals(false);
                   }}
-                  className="history cursor-pointer p-3 tracking-[4px] w-[260px]  text-[20px] bg-[#dae2ec] focus:bg-white transition-all duration-500 hover:bg-blue-100 hover:text-blue-400"
+                  className={`cursor-pointer p-3 tracking-[4px] w-full sm:w-[260px] text-[16px] lg:text-[20px] bg-[#dae2ec] ${
+                    history ? "bg-white" : ""
+                  } transition-all duration-500 hover:bg-blue-100 hover:text-blue-400`}
                 >
                   history
                 </button>
@@ -39,7 +40,9 @@ function App() {
                     setVision(true);
                     setGoals(false);
                   }}
-                  className="vision cursor-pointer  tracking-[4px] w-[260px]  text-[20px] bg-[#dae2ec] focus:bg-white transition-all duration-500 hover:bg-blue-100 hover:text-blue-400 "
+                  className={`cursor-pointer p-3 tracking-[4px] w-full sm:w-[260px] text-[16px] lg:text-[20px] bg-[#dae2ec] ${
+                    vision ? "bg-white" : ""
+                  } transition-all duration-500 hover:bg-blue-100 hover:text-blue-400`}
                 >
                   vision
                 </button>
@@ -49,25 +52,19 @@ function App() {
                     setVision(false);
                     setGoals(true);
                   }}
-                  className="
-                  goals cursor-pointer tracking-[4px] w-[260px]  text-[20px] bg-[#dae2ec] focus:bg-white transition-all duration-500 hover:bg-blue-100 hover:text-blue-400"
+                  className={`cursor-pointer p-3 tracking-[4px] w-full sm:w-[260px] text-[16px] lg:text-[20px] bg-[#dae2ec] ${
+                    goals ? "bg-white" : ""
+                  } transition-all duration-500 hover:bg-blue-100 hover:text-blue-400`}
                 >
                   goals
                 </button>
               </div>
               <div className={`${history ? "" : "hidden"}`}>
-                <h2 className=" font-semibold text-[20px] px-4 pt-2 tracking-[4px] text-gray-800">
+                <h2 className="font-semibold text-[20px] px-4 pt-2 tracking-[4px] text-gray-800">
                   history
                 </h2>
                 <p className="w-full p-5 text-gray-600">
-                  I'm baby wolf pickled schlitz try-hard normcore marfa man bun
-                  mumblecore vice pop-up XOXO lomo kombucha glossier bicycle
-                  rights. Umami kinfolk salvia jean shorts offal venmo.
-                  Knausgaard tilde try-hard, woke fixie banjo man bun. Small
-                  batch tumeric mustache tbh wayfarers 8-bit shaman chartreuse
-                  tacos. Viral direct trade hoodie ugh chambray, craft beer pork
-                  belly flannel tacos single-origin coffee art party migas plaid
-                  pop-up.
+                  I'm baby wolf pickled schlitz try-hard normcore marfa man bun mumblecore vice pop-up XOXO lomo kombucha glossier bicycle rights. Umami kinfolk salvia jean shorts offal venmo. Knausgaard tilde try-hard, woke fixie banjo man bun. Small batch tumeric mustache tbh wayfarers 8-bit shaman chartreuse tacos. Viral direct trade hoodie ugh chambray, craft beer pork belly flannel tacos single-origin coffee art party migas plaid pop-up.
                 </p>
               </div>
               <div className={`${vision ? "" : "hidden"}`}>
@@ -75,15 +72,7 @@ function App() {
                   vision
                 </h2>
                 <p className="w-full p-5 text-gray-600">
-                  Man bun PBR&B keytar copper mug prism, hell of helvetica.
-                  Synth crucifix offal deep v hella biodiesel. Church-key
-                  listicle polaroid put a bird on it chillwave palo santo enamel
-                  pin, tattooed meggings franzen la croix cray. Retro yr
-                  aesthetic four loko tbh helvetica air plant, neutra palo santo
-                  tofu mumblecore. Hoodie bushwick pour-over jean shorts
-                  chartreuse shabby chic. Roof party hammock master cleanse
-                  pop-up truffaut, bicycle rights skateboard affogato readymade
-                  sustainable deep v live-edge schlitz narwhal.
+                  Man bun PBR&B keytar copper mug prism, hell of helvetica. Synth crucifix offal deep v hella biodiesel. Church-key listicle polaroid put a bird on it chillwave palo santo enamel pin, tattooed meggings franzen la croix cray. Retro yr aesthetic four loko tbh helvetica air plant, neutra palo santo tofu mumblecore. Hoodie bushwick pour-over jean shorts chartreuse shabby chic. Roof party hammock master cleanse pop-up truffaut, bicycle rights skateboard affogato readymade sustainable deep v live-edge schlitz narwhal.
                 </p>
               </div>
               <div className={`${goals ? "" : "hidden"}`}>
@@ -91,15 +80,7 @@ function App() {
                   Goals
                 </h2>
                 <p className="w-full p-5 text-gray-600">
-                  Chambray authentic truffaut, kickstarter brunch taxidermy vape
-                  heirloom four dollar toast raclette shoreditch church-key.
-                  Poutine etsy tote bag, cred fingerstache leggings cornhole
-                  everyday carry blog gastropub. Brunch biodiesel sartorial
-                  mlkshk swag, mixtape hashtag marfa readymade direct trade man
-                  braid cold-pressed roof party. Small batch adaptogen coloring
-                  book heirloom. Letterpress food truck hammock literally hell
-                  of wolf beard adaptogen everyday carry. Dreamcatcher pitchfork
-                  yuccie, banh mi salvia venmo photo booth quinoa chicharrones.
+                  Chambray authentic truffaut, kickstarter brunch taxidermy vape heirloom four dollar toast raclette shoreditch church-key. Poutine etsy tote bag, cred fingerstache leggings cornhole everyday carry blog gastropub. Brunch biodiesel sartorial mlkshk swag, mixtape hashtag marfa readymade direct trade man braid cold-pressed roof party. Small batch adaptogen coloring book heirloom. Letterpress food truck hammock literally hell of wolf beard adaptogen everyday carry. Dreamcatcher pitchfork yuccie, banh mi salvia venmo photo booth quinoa chicharrones.
                 </p>
               </div>
             </div>
@@ -111,3 +92,4 @@ function App() {
 }
 
 export default App;
+
