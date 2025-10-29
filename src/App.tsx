ca7cc9753d6a37b3b0611669b5137750a0d86299
minabayat <minabayatt9@gmail.com>
2025-10-29 14:23:37 +0330

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,20 @@ function App() {
 
   return (
     <>
-      <div className=" h-full flex flex-col justify-center items-center gap-5">
-        <h2 className="text-5xl font-bold text-gray-800">About</h2>
-        <p className="text-gray-600 text-[20px]">
+      <div className=" h-full flex lg:flex-col flex-wrap justify-center items-center gap-5">
+        <h2 className="lg:text-4xl tracking-[2px] text-3xl font-semibold text-gray-800">About</h2>
+        <p className="text-gray-600 text-base lg:text-[20px]">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
           aperiam!
         </p>
-        <div className="flex flex-row gap-5 mt-5">
+        <div className="flex flex-col gap-5 lg:flex-row mt-5">
           <img
-            className="w-[569px] h-[300px] rounded-lg"
+            className="lg:w-[569px] w-full h-[300px] rounded-lg"
             src=".\src\assets\img\hero-bcg.1486d4061cd0252b9a21.jpeg"
             alt=""
           />
           <div className="flex flex-col">
-            <div className="bg-white w-[570px] h-[300px] rounded-lg ">
+            <div className="bg-white lg:w-[570px] w-full h-auto rounded-lg ">
               <div className="flex flex-row justify-around">
                 <button
                   onClick={() => {
@@ -29,7 +29,7 @@ function App() {
                     setVision(false);
                     setGoals(false);
                   }}
-                  className="${history?} cursor-pointer p-3 tracking-[4px] w-[260px]  text-[20px] bg-[#dae2ec] focus:bg-white transition-all duration-500 hover:bg-blue-100 hover:text-blue-400"
+                  className="history cursor-pointer p-3 tracking-[4px] w-[260px]  text-[20px] bg-[#dae2ec] focus:bg-white transition-all duration-500 hover:bg-blue-100 hover:text-blue-400"
                 >
                   history
                 </button>
@@ -39,7 +39,7 @@ function App() {
                     setVision(true);
                     setGoals(false);
                   }}
-                  className="${vision?} cursor-pointer  tracking-[4px] w-[260px]  text-[20px] bg-[#dae2ec] focus:bg-white transition-all duration-500 hover:bg-blue-100 hover:text-blue-400 "
+                  className="vision cursor-pointer  tracking-[4px] w-[260px]  text-[20px] bg-[#dae2ec] focus:bg-white transition-all duration-500 hover:bg-blue-100 hover:text-blue-400 "
                 >
                   vision
                 </button>
@@ -49,7 +49,8 @@ function App() {
                     setVision(false);
                     setGoals(true);
                   }}
-                  className="${goals?} cursor-pointer tracking-[4px] w-[260px]  text-[20px] bg-[#dae2ec] focus:bg-white transition-all duration-500 hover:bg-blue-100 hover:text-blue-400"
+                  className="
+                  goals cursor-pointer tracking-[4px] w-[260px]  text-[20px] bg-[#dae2ec] focus:bg-white transition-all duration-500 hover:bg-blue-100 hover:text-blue-400"
                 >
                   goals
                 </button>
